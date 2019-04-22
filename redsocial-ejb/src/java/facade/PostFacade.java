4ccb0bae4fb0c11896ffba27c3ae124927d8fb88
MykexMP <MykexMP@hotmail.es>
2019-04-22 23:20:05 +0200

    Posibles modificaciones a consultar con edu de la consulta.

--- redsocial-ejb/src/java/facade/PostFacade.java
+++ redsocial-ejb/src/java/facade/PostFacade.java
@@ -34,6 +34,8 @@ public class PostFacade extends AbstractFacade<Post> {
     
     public List<Post> findAllPostMainWindow(User u){
         Query q = em.createQuery("SELECT p FROM Post p WHERE (p.visibility.typeVis = 'Public') or (p.creator = :u) ORDER BY p.datePost DESC"); // Edit Query
+        // or (:u2 MEMBER OF p.creator.friendList AND p.visibility.typeVis = 'Friends')
+        // or (?? AND p.visibility.typeVis = 'Group')
         q.setParameter("u", u);
         return q.getResultList();
     }
